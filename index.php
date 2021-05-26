a7dde3a36559b8d3cd06049d156597c2be0cb8f1
christopher.porreca <chris@porreca.dev>
2021-05-26 18:50:02 -0400

    adding factory image to index.php to test basic template

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>[html Factory]</title>
 </head>
 <body bgcolor=103050 alink=ffd800 vlink=ffd800 link=ffd800 text=ffffff>
-  Hello, World!
+  <img src=factory.png width=50><br>Hello, World!
 </body>
 </html>
