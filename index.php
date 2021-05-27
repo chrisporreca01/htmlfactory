01e2b2372f5d05a916aa44a73628072f483f8aa7
christopher.porreca <chris@porreca.dev>
2021-05-26 21:05:08 -0400

    update logo htmlf.png with table structure

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
 </head>
 <body bgcolor=103050 alink=ffd800 vlink=ffd800 link=ffd800 text=ffffff>
   <img src=factory.png width=50><br>Hello, World!
-  <table width=80% bgcolor=ffd800>
+  <table width=100% bgcolor=ffd800>
     <tr>
-      <td align=center>
+      <td align=right>
         <img src=htmlf.png width=50%>
       </td>
     </tr>
