ce63061060ce9e947aad02ed9a62e27a5f056d6d
Chris Porreca <33973086+chrisporreca01@users.noreply.github.com>
2021-05-27 11:54:17 -0400

    adding css
    adding bootstrap css // slate

--- index.php
+++ index.php
@@ -1,25 +1,33 @@
-<html>
-<head>
-  <title>[html Factory]</title>
-</head>
-<body bgcolor=103050 alink=ffd800 vlink=ffd800 link=ffd800 text=ffffff>
-  <table width=100% bgcolor=ffd800>
-    <tr>
-      <td>
-        <table bgcolor=ffd800>
-          <tr>
-            <td>
-              <img src=factoryshadow.png height=50>
-            </td>
-            <td>
-              <img src=fullfacts.png height=50>
-            </td>
-          </tr>
-        </table>
-      </td>
-    </tr>
-  </table>
-  <br>
-  <a href=mailto:support@htmlfactory.online>support@htmlfactory.online</a> feel free to contact support@htmlfactory.com !
-</body>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="refresh" content="600">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="slate.b.css">
+    <link rel="stylesheet" href="slate.b.min.css">
+    <title>[html Factory]</title>
+  </head>
+  <body>
+    <h1>[html Factory]</h1>
+    <table width=100% bgcolor=ffd800>
+      <tr>
+        <td>
+          <table bgcolor=ffd800>
+            <tr>
+              <td>
+                <img src=factoryshadow.png height=50>
+              </td>
+              <td>
+                <img src=fullfacts.png height=50>
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+    </table>
+    <a href=mailto:support@htmlfactory.online>support@htmlfactory.online</a> feel free to contact support@htmlfactory.com
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+  </body>
 </html>
