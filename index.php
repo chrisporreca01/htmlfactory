f6cbf2b7b38a4e3912e0ee877528888aee91bad7
christopher.porreca <chris@porreca.dev>
2021-05-26 21:06:36 -0400

    update hello world

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <title>[html Factory]</title>
 </head>
 <body bgcolor=103050 alink=ffd800 vlink=ffd800 link=ffd800 text=ffffff>
-  <img src=factory.png width=50><br>Hello, World!
+  <img src=factory.png width=50>
   <table width=100% bgcolor=ffd800>
     <tr>
       <td align=right>
@@ -11,5 +11,7 @@
       </td>
     </tr>
   </table>
+  <br>
+  Hello, World!
 </body>
 </html>
