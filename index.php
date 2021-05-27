d1639ace5cae851aa6dfcc1c269bca514a1761b1
Chris Porreca <33973086+chrisporreca01@users.noreply.github.com>
2021-05-27 18:30:57 -0400

    fixed menu
    more specific menu 1

--- index.php
+++ index.php
@@ -95,23 +95,23 @@ a:active { text-decoration: none; }
       <div class="sticky-top">
         <ul class="nav nav-pills">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Active</a>
+            <a class="nav-link active" aria-current="page" href="#htmlfactory">html Factory</a>
           </li>
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Dropdown</a>
+            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Account Management</a>
             <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
+              <li><a class="dropdown-item" href="#create">Create Account</a></li>
+              <li><a class="dropdown-item" href="#login">Login</a></li>
+              <li><a class="dropdown-item" href="#dashboard">Account Dashboard</a></li>
               <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Separated link</a></li>
+              <li><a class="dropdown-item" href="#logout">Logout</a></li>
             </ul>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="#services">Services</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            <a class="nav-link disabled" href="mailto:support@htmlfactory.online" tabindex="-1" aria-disabled="true">support@htmlfactory.online</a>
           </li>
         </ul>
       </div>
