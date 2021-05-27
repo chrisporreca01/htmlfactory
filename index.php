e5fefd9325ab46a2352abbb620844bd502bfebb7
christopher.porreca <chris@porreca.dev>
2021-05-26 22:18:43 -0400

    factoryshadow.png

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <title>[html Factory]</title>
 </head>
 <body bgcolor=103050 alink=ffd800 vlink=ffd800 link=ffd800 text=ffffff>
-  <img src=factorycy.png width=50>
+  <img src=factoryshadow.png width=50>
   <table width=100% bgcolor=ffd800>
     <tr>
       <td>
